bb8962ecd661ccc918014f46536f4f5b21b909db
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-02-04 08:17:14 +0530

    Initial User Model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -22,6 +24,21 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// userSchema.methods.generateAuthTocken = function () {
-//   const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY,{expiresIn:"7d"});
-// };
+userSchema.methods.generateAuthTocken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { User, validate };
